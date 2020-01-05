c1744835a3a418229f0cd7dcc653d395d79971a7
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-05 21:37:36 +0700

    Refactor 1

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,12 @@
 // const company = new Company();
 // console.log(company);
 import { User } from './User';
+import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
 const user = new User();
+const company = new Company();
 const customMap = new CustomMap('map');
 
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
